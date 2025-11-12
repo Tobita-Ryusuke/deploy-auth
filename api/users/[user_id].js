8935db8db6c864497e5246e1efc103ed9a51d0ff
tobi <c1319855@st.kanazawa-it.ac.jp>
2025-11-12 19:07:47 +0900

    エラーの修正3

--- api/users/[user_id].js
+++ api/users/[user_id].js
@@ -1,4 +1,3 @@
-const users = require("../_data");
 const checkAuth = require("../_auth");
 
 module.exports = async (req, res) => {
@@ -9,10 +8,6 @@ module.exports = async (req, res) => {
     return res.status(401).json({ message: "Authentication failed" });
   }
 
-  if (!users[user_id]) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
   if (req.method === "GET") {
     return res.status(200).json({ message: "User details by user_id" });
   }
@@ -21,10 +16,6 @@ module.exports = async (req, res) => {
     if (auth.user_id !== user_id) {
       return res.status(403).json({ message: "No permission for update" });
     }
-    const { nickname, comment, password } = req.body || {};
-    if (nickname) users[user_id].nickname = nickname;
-    if (comment) users[user_id].comment = comment;
-    if (password) users[user_id].password = password;
 
     return res.status(200).json({ message: "User successfully updated" });
   }
